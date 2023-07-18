fbd1f17b30a872d83d64aec9632b2d47e287c441
Shardul Vaidya <vshardul@amazon.com>
2023-07-18 16:08:16 -0400

    Added ObserverBot gitignore

--- eks-anywhere-common/Addons/Core/Kube-Observer/ObserverBot/bot.py
+++ eks-anywhere-common/Addons/Core/Kube-Observer/ObserverBot/bot.py
@@ -1,6 +1,11 @@
 import os
 from kubernetes import client, config
 
+# https://github.com/kubernetes-client/python
+# Using the kubernetes client to get the exact information
+# we need to write the feedback bot
+def run_observer():
+    pass
 
 
 if __name__ == "__main__":
