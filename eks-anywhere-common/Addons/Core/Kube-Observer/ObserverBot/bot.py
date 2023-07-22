e1bbe2ffcf996da427f25bed030d32ba1fe8948e
Shardul Vaidya <vshardul@amazon.com>
2023-07-22 15:02:36 -0400

    Added report generation into the system, still testing it

--- eks-anywhere-common/Addons/Core/Kube-Observer/ObserverBot/bot.py
+++ eks-anywhere-common/Addons/Core/Kube-Observer/ObserverBot/bot.py
@@ -16,9 +16,9 @@ def run_observer():
 
     risky_pods = [ get_at_risk_pods(i) for i in all_namespaces.items ]
 
-    risky_deployments = [ get_at_risk_deployments(i.pod) for i in risky_pods ]
+    risky_deployments = [ get_at_risk_deployments(i) for i in risky_pods if i ]
 
-    reports = [ build_report(i) for i in risky_deployments ]
+    reports = [build_report(i) for i in risky_deployments]
 
     # If we wanna go F A S T - maybe
     # with concurrent.futures.ThreadPoolExecutor(max_workers=len(all_namespaces)) as observer:
@@ -47,6 +47,7 @@ def get_at_risk_pods(ns: V1Namespace) -> []:
         # If the pod isn't in a "Success/Pending" state, immediately flag it
         if pod_status.phase not in { "Running", "Pending", "Succeeded" }:
             at_risk_pods.append({
+                "ns": ns.metadata.name,
                 "pod": pod,
                 "reason_type": V1PodStatus,
                 "reason": pod.status
@@ -59,16 +60,10 @@ def get_at_risk_pods(ns: V1Namespace) -> []:
             # If state is CrashLoopBackoff - definitely bad
             # If state is ImagePullError - definitely bad
             # If state is Error - definitely bad
-            if not status.ready:
+            # Something is bad and the pod has restarted a bunch of times
+            if not status.ready and status.restart_count >= 1:
                 at_risk_pods.append({
-                    "pod": pod,
-                    "reason_type": V1ContainerStatus,
-                    "reason": status
-                })
-                continue
-            # The container has restarted >= 1 time
-            if status.restart_count >= 1:
-                at_risk_pods.append({
+                    "ns": ns.metadata.name,
                     "pod": pod,
                     "reason_type": V1ContainerStatus,
                     "reason": status
@@ -76,17 +71,53 @@ def get_at_risk_pods(ns: V1Namespace) -> []:
                 continue
 
     # If delta in replicas and ready replicas - this might lead to false positives
-    pass
+    return at_risk_pods
 
 
 """
     Drill up from the failing pod level to the deployment that's causing the trouble
 """
-def get_at_risk_deployments(pod: kubernetes.client.V1Pod) -> [kubernetes.client.V1Deployment]:
+def get_at_risk_deployments(risk_report):
+    # risk_report is [risk]
     # metadata.owner_references.name - to get the owner of a pod
     # Find out the exact owners of the pods that are failing
 
-    pass
+    deployment_ns = risk_report[0]["ns"]
+
+    # If the containers belong to the same replica sets, they
+    # belong to the same deployment?
+    at_risk_replica_sets = {}
+
+    at_risk_deployments = []
+
+    # Get at risk replica sets
+    for risk in risk_report:
+        pod_owner = risk["pod"].metadata.owner_references[0].name
+
+        if pod_owner in at_risk_replica_sets:
+            at_risk_replica_sets[pod_owner].append(risk)
+        else:
+            at_risk_replica_sets[pod_owner] = [risk]
+
+    for replica_set in at_risk_replica_sets:
+        # Gets the replica set associated with the at risk pod
+        replica_set_info = apps_api.read_namespaced_replica_set(replica_set, deployment_ns)
+
+        replica_owner = replica_set_info.metadata.owner_references[0].name
+        deployment = apps_api.read_namespaced_deployment(replica_owner, deployment_ns)
+
+        # Might have to deep copy this one
+        at_risk = {
+            "deployment": deployment,
+            "ns": deployment_ns,
+            "risks": [
+                at_risk_replica_sets[replica_set]
+            ]
+        }
+
+        at_risk_deployments.append(at_risk)
+
+    return at_risk_deployments
 
 
 """
@@ -101,15 +132,25 @@ def get_pod_logs(pod: kubernetes.client.V1Pod):
     Build the Github PR comment here, providing all the information they would need to
     debug the problem (hopefully)
 """
-def build_report(risk):
+def build_report(risk_info):
     # risk:
     #   deployment: V1Deployment
-    #   pod: V1Pod
-    #   reason_type: Any
-    #   reason: typeof(reason_type)
+    #   ns: String
+    #   risk:
+    #     ns: String
+    #     pod: V1Pod
+    #     reason_type: Any
+    #     reason: typeof(reason_type)
+    for risk in risk_info:
+        report = f"Looks like your deployment `{risk['deployment'].metadata.name}` is failing.\n" \
+                 f"Specifically, it looks like these pods are failing: \n"
+        for pod_risk in risk["risks"]:
+            report += f"* Pod: `{pod_risk['pod'].metadata.name}` with the error listed below.\n"
 
+        for pod_risk in risk["risks"]:
+            report += f"Logs for pod `{pod_risk['pod'].metadata.name}`: ```{get_pod_logs(pod_risk.pod)}```\n ---- "
 
-    pass
+    return report
 
 """
     Get Issue Number from the namespace
