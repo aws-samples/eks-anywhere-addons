bc31e263601ae71bc20b1f5fb9bbd963696bd199
Shardul Vaidya <vshardul@amazon.com>
2023-09-07 23:26:17 -0400

    consider it done, testing in my env before pushing

---
+++ eks-anywhere-common/Addons/Core/Kube-Observer/ci-infra/bin/ci-infra.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CiInfraStack } from '../lib/ci-infra-stack';
+
+const app = new cdk.App();
+new CiInfraStack(app, 'CiInfraStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  env: { account: '867286930927', region: 'us-west-2' },
+
+});
